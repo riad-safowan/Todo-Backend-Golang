29aae87108aa71a41cc08ef5cf11f122032125c0
Riad Safowan <riad.safowan@northsouth.edu>
2022-01-24 15:49:33 +0600

    get tasks and task by id

--- controllers/task.go
+++ controllers/task.go
@@ -23,8 +23,6 @@ func AddTask() gin.HandlerFunc {
 		var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 		var task models.Task
-		task.ID = primitive.NewObjectID()
-		task.Task_id = task.ID.Hex()
 		var tasks []models.Task
 		var user_id = c.GetString("user_id")
 
@@ -40,6 +38,8 @@ func AddTask() gin.HandlerFunc {
 			defer cancel()
 			return
 		}
+		task.ID = primitive.NewObjectID()
+		task.Task_id = task.ID.Hex()
 
 		projection := bson.D{
 			{"_id", 0},
@@ -56,7 +56,7 @@ func AddTask() gin.HandlerFunc {
 				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 				return
 			}
-		
+
 		}
 
 		_, insertErr := taskCollection.UpdateOne(ctx, bson.M{"user_id": user_id}, bson.D{{"$push", bson.D{{"tasks", task}}}}, options.Update().SetUpsert(true))
@@ -108,8 +108,8 @@ func AddTasks() gin.HandlerFunc {
 
 			} else {
 				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
 			}
-			return
 		}
 
 		oldTasks = append(oldTasks, validatedTask...)
@@ -119,19 +119,59 @@ func AddTasks() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"error": insertErr.Error()})
 			return
 		}
-
 		c.JSON(http.StatusOK, oldTasks)
 	}
 }
 
 func GetTask() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		taskId := c.Param("task_id")
+		id, _ := primitive.ObjectIDFromHex(taskId)
+		println(taskId)
 
+		projection := bson.D{
+			{"_id", 0},
+			{"user_id", 0},
+			{"tasks", bson.D{
+				{"$elemMatch", bson.D{{"_id", id}}},
+			}},
+		}
+
+		var response models.Tasks
+		err := taskCollection.FindOne(ctx, bson.M{"tasks._id": id},
+			options.FindOne().SetProjection(projection),
+		).Decode(&response)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		task := response.Tasks[0]
+		c.JSON(http.StatusOK, task)
 	}
 }
 
 func GetTasks() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		var user_id = c.GetString("user_id")
 
+		projection := bson.D{
+			{"_id", 0},
+			{"tasks", 1},
+		}
+
+		var response models.Tasks
+		err := taskCollection.FindOne(ctx, bson.M{"user_id": user_id}, options.FindOne().SetProjection(projection)).Decode(&response)
+		tasks := response.Tasks
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, tasks)
 	}
+
 }
--- routes/task.go
+++ routes/task.go
@@ -8,7 +8,7 @@ import (
 
 func Task(r *gin.Engine) {
 r.Use(middleware.Authenticate())
-r.GET("/user/task", controllers.GetTask())
+r.GET("/user/task/:task_id", controllers.GetTask())
 r.GET("/user/tasks", controllers.GetTasks())
 r.POST("/user/task", controllers.AddTask())
 r.POST("/user/tasks", controllers.AddTasks())
